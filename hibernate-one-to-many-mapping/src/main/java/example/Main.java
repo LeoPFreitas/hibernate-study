7a9f09237b250fc3bb139a426bbe609996537c11
LeoPFreitas <leonardo.cadu2@gmail.com>
2021-01-03 17:33:40 -0300

    fix typo

--- hibernate-one-to-many-mapping/src/main/java/example/Main.java
+++ hibernate-one-to-many-mapping/src/main/java/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
-            getEmployAndAddressByEMployeeId();
+            getEmployAndAddressByEmployeeId();
 
 //            session.beginTransaction();
 //            session.persist(getEmployee());
@@ -48,12 +48,13 @@ public class Main {
         return employee;
     }
 
-    private static void getEmployAndAddressByEMployeeId() {
+    private static void getEmployAndAddressByEmployeeId() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Employee employee = session.get(Employee.class, 1);
 
             if (employee != null) {
                 System.out.println(employee.toString());
+
                 employee.getAddressList()
                         .forEach(e -> System.out.println(e.toString()));
             }
